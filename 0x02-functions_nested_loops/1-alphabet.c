ac07fb42272f4936e68f9785192fcc5d25dcb105
MosabAboidrees <mosab_mahala@yahoo.com>
2023-09-12 15:38:33 -0700

    prints the alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,16 +3,20 @@
 /**
  * main - Entry point
  *
- * Description: a function that prints the alphabet, in lowercase
+ * Description: a Prototype function that prints the
+ * alphabet, in lowercase followed by a new line
  *
  * Return: Always 0 (Success)
  *
 */
+
+/* The Prototype function start here*/
 void print_alphabet(void)
 {
 	int i;
 
 	for (i = 'a'; i <= 'z'; i++)
-		_putchar(i);
+		_putchar(i); /* print the current charactor*/
 	_putchar('\n');
+	return (0);
 }
