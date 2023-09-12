433d264f092483b9da36797d29f870201845bb5d
MosabAboidrees <mosab_mahala@yahoo.com>
2023-09-12 15:32:46 -0700

    prints the alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,18 +2,17 @@
 
 /**
  * main - Entry point
- * 
- * Description: _putchar - writes the character c to stdout
+ *
+ * Description: a function that prints the alphabet, in lowercase
  *
  * Return: Always 0 (Success)
  *
 */
-
 void print_alphabet(void)
 {
 	int i;
 
-	for (i = 'a'; i <='z'; i++)
+	for (i = 'a'; i <= 'z'; i++)
 		_putchar(i);
 	_putchar('\n');
 }
