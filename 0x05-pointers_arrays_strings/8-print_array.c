53a26973ce36e6c45dfc617ab37f6dd89899b10e
MosabAboidrees <mosab_mahala@yahoo.com>
2023-09-18 14:47:07 -0700

    prints elements of an array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,7 +12,7 @@ void print_array(int *a, int n)
 {
 	int count;
 
-	for (count= 0; count < n; count++)
+	for (count = 0; count < n; count++)
 		if (count != (n - 1))
 			printf("%d, ", a[count]);
 		else
