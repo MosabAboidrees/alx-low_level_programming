b3d230b2f4ce4a0d995a452aa12f3513f65c98f6
MosabAboidrees <mosab_mahala@yahoo.com>
2023-09-18 15:56:18 -0700

    convert a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,15 +11,15 @@ int _atoi(char *s)
 {
 	unsigned int num = 0;
 	int sign = 1;
-	int i = 0;
 
-	while (s[i])
-		if (s[i] == '-')  /* if s negative */
+	do {
+		if (*s == '-')  /* if s negative */
 			sign *= -1;
-		else if (s[i] >= '0' && s[i] <= '9')
-			num = (num * 10) + (s[i] - '0');
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
 		else if (num > 0)
 			break;
-	i++;
+	} while (*s++);
+
 	return (num * sign);
 }
