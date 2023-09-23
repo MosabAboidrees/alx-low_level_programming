d4db43ae07251e3f9ba720d0f7cb476618718b1c
MosabAboidrees <mosab_mahala@yahoo.com>
2023-09-22 19:03:17 -0700

    prints an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,8 +2,7 @@
 
 /**
  * print_number - Prints an integer using only _putchar.
- * @num: The integer to be printed.
- * @integ: used to convert negative numbers
+ * @n: The integer to be printed.
  *
  * Description: This function takes an integer as input and prints it
  * character by character using the _putchar function.
@@ -13,19 +12,21 @@
  * mosab_mahala@yahoo.com
 */
 
-void print_number(int num)
+void print_number(int n)
 {
 	unsigned int integ; /* Used for handling negative numbers */
 
-	integ = num;
-	if (num < 0)
+	integ = n;
+	if (n < 0)
 	{
 		_putchar('-'); /* Print '-' for negative numbers */
-		integ = -num;  /* Convert negative number to positive */
+		integ = -n;  /* Convert negative number to positive */
 	}
 	if (integ / 10 != 0)
 	{
-		print_number(integ / 10); /* Recursive call for digits before the last one */
+		/* Recursive call for digits before the last one */
+		print_number(integ / 10);
 	}
-	_putchar((integ % 10) + '0'); /* Print the last digit as a character */
+	/* Print the last digit as a character */
+	_putchar((integ % 10) + '0');
 }
