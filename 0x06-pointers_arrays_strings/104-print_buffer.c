98aedc4f1175976aea557685eee98e3f6b2c0093
MosabAboidrees <mosab_mahala@yahoo.com>
2023-09-22 17:28:39 -0700

    prints a buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,49 +1,91 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * print_buffer - Prints the content of a buffer in a specified format.
+ * is_ASCII_Printable - This function checks if the given
+ * ASCII value 'n' represents a printable character within
+ * the ASCII range (from 32 to 126).
+ * @n: The ASCII value to check.
+ * Returns 1 if it's printable, 0 otherwise.
+ */
+int is_ASCII_Printable(int n)
+{
+	return (n >= 32 && n <= 126);
+}
+/**
+ * print_Hexadecimal - Prints the hexadecimal representation
+ * of bytes in a buffer segment. starting from index
+ * 'start' to 'end'. It prints 10 bytes per line.
+ * @buffer: The pointer to the buffer.
+ * @start: The starting index of bytes to print.
+ * @end: The ending index of bytes to print.
+ */
+void print_Hexadecimal(char *buffer, int start, int end)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (i < end)
+			printf("%02x", *(buffer + start + i)); /* Print two hexadecimal digits*/
+		else
+			printf("  "); /* Pad with spaces for incomplete lines*/
+		if (i % 2)
+			printf(" "); /* Add a space every two bytes*/
+		i++;
+	}
+}
+/**
+ * print_ASCII - Prints the ASCII representation of bytes
+ * in a buffer segment. Non-printable characters are represented
+ * as '.' (period).
+ * @buffer: The pointer to the buffer.
+ * @start: The starting index of bytes to print.
+ * @end: The ending index of bytes to print.
+ */
+void print_ASCII(char *buffer, int start, int end)
+{
+	int ch, i = 0;
+
+	while (i < end)
+	{
+		ch = *(buffer + i + start);
+		if (!is_ASCII_Printable(ch))
+			ch = '.'; /*Replace non-printable characters with '.'*/
+		printf("%c", ch); /* Print the character*/
+		i++;
+	}
+}
+/**
+ * print_buffer - This function takes a pointer to a buffer
+ * and the number of bytes 'size' to be printed.
+ * It prints the content of the buffer in a specific format, with
+ * 10 bytes per line, including hexadecimal and ASCII representations.
  * @buffer: The pointer to the buffer to be printed.
  * @size: The number of bytes to be printed from the buffer.
-*/
+ */
 void print_buffer(char *buffer, int size)
 {
-	int offset = 0;  /*Initialize the offset to 0*/
-	int bytesToPrint; /*Initialize to store the number of bytes to print*/
+	int start, end;
 
-	if (size <= 0)
+	if (size > 0)
 	{
-		printf("\n"); /*If size is 0 or less, print a newline and return*/
-		return;
-	}
-	while (offset < size)
-	{
-		/*Calculate the number of bytes to print in this iteration*/
-		bytesToPrint = (size - offset < 10) ? (size - offset) : 10;
-		printf("%08x: ", offset); /*Print the offset in hexadecimal*/
-		/* Print hexadecimal content and add spaces for formatting*/
-		for (int i = 0; i < 10; i++)
+		for (start = 0; start < size; start += 10)
 		{
-			if (i < bytesToPrint)
-				/*Print two hexadecimal digits*/
-				printf("%02x", *(buffer + offset + i));
-			else
-				/*Pad with spaces for incomplete lines*/
-				printf("  ");
-			if (i % 2)
-				printf(" "); /*Add a space every two bytes*/
-		}
-		/*Print the character representation of the bytes*/
-		for (int i = 0; i < bytesToPrint; i++)
-		{
-			int character = *(buffer + offset + i);
-
-			if (character < 32 || character > 126)
+			if (size - start < 10)
 			{
-				/* Replace non-printable characters with '.'*/
-				character = '.';
+				end = size - start; /*number of bytes to print in this iteration*/
 			}
-			printf("%c", character);
+			else
+			{
+				end = 10;
+			}
+			printf("%08x: ", start); /*Print the offset in hexadecimal*/
+			print_Hexadecimal(buffer, start, end);
+			print_ASCII(buffer, start, end);
+			printf("\n");
 		}
-		printf("\n"); /*Move to the next line*/
-		offset += 10; /*Increment the offset for the next iteration*/
+	}
+	else
+	{
+		printf("\n"); /*Print a newline if size is 0 or less*/
 	}
 }
