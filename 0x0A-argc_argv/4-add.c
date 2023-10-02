03a321abf92b69c0aefbc767e03db297676972b8
MosabAboidrees <mosab_mahala@yahoo.com>
2023-10-02 13:08:15 -0700

    adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,42 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-/**
- * is_positive_number - Check if a a positive number
- * @str: String to check
- * Return: 1 means not positive number or not digit, 0 otherwise
-*/
-int is_positive_number(char *str)
-{
-	if (*str == '-')
-		return (1); /* Negative numbers are not allowed*/
-	while (*str)
-	{
-		if (!isdigit(*str))
-			return (1); /*Non-digit not allowed*/
-		str++;
-	}
-	return (0);
-}
 /**
  * main - a program that adds positive numbers
  * @argc: Number of input arguments
  * @argv: Array of input argument strings
  * Return: 0 (Success) or 1 (Error)
 */
+#include <stdio.h>
+#include <stdlib.h>
+
 int main(int argc, char *argv[])
 {
 	int sum = 0;
 
-	if (argc == 1) /* Check if no numbers are passed*/
+	if (argc == 1)
+	{
 		printf("0\n");
 		return (0);
+	}
 	for (int i = 1; i < argc; i++)
 	{
-		/* Check if input argument is a positive number,*/
-		/*then, calculate the sum of positive numbers */
-		is_positive_number(argv[i]) ? (sum += atoi(argv[i])) :
-			return (printf("Error\n"), 1);
+		char *c = argv[i];
+
+		while (*c)
+		{
+			if (*c < '0' || *c > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			c++;
+		}
+		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 	return (0);
