83b6e3c8db1dd3532c5884c584383580d98bf1b6
MosabAboidrees <mosab_mahala@yahoo.com>
2023-10-02 14:39:37 -0700

    make change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -20,22 +20,13 @@ int main(int argc, char *argv[])
 		return (1); /*Return error*/
 	}
 	cents = atoi(argv[1]); /* Convert to an integer*/
-	/* Check if the input argument is negative */
-	if (cents < 0)
-	{
-		printf("0\n");
-		return (0); /* Return 0 for negative input*/
-	}
 	numCoins = sizeof(coins) / sizeof(coins[0]);
 	minCoins = 0;
 	/* Apply Greedy algorithm to calculate the minimum number of coins */
 	for (int i = 0; i < numCoins && cents >= 0; i++)
 	{
-		while (cents >= coins[i])
-		{
-			minCoins = minCoins + (cents / coins[i]);
-			cents = cents % coins[i];
-		}
+		minCoins = minCoins + (cents / coins[i]);
+		cents = cents % coins[i];
 	}
 	/* Print the minimum number of coins*/
 	printf("%d\n", minCoins);
