b152bf777a5e96e6bf86ca455692ca6121cda847
MosabAboidrees <mosab_mahala@yahoo.com>
2023-10-08 15:45:56 -0700

    allocates memory using malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,10 @@
-#include <stdlib.h>
 #include "main.h"
 
 /**
  * *malloc_checked - a function that allocates memory using malloc.
- * if malloc fails, the malloc_checked function should cause normal
- * process termination with a status value of 98
+ *
  * @b: the size of the memory to allocate
+ *
  * Return: a pointer to the allocated memory
 */
 
