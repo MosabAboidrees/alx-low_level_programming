e56ef5aa26b7c30fcd9eeee338deeb256ee85d94
MosabAboidrees <mosab_mahala@yahoo.com>
2023-10-09 12:58:20 -0700

    allocates memory using malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,15 +5,14 @@
  * @b: size of memory.
  * Return: exits with a status value of 98 if fails. void pointer if succesfull
  */
-
 void *malloc_checked(unsigned int b)
 {
-        void *mem_ptr;
+	void *mem_ptr;
 
-        mem_ptr = malloc(b);
-
-        if (!mem_ptr)
-                exit(98);
-
-        return (mem_ptr);
+	mem_ptr = malloc(b);
+	if (mem_ptr == NULL)
+	{
+		exit(98);
+	}
+	return (mem_ptr);
 }
