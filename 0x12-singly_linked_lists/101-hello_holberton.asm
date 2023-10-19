168491430830faf1e51dac8f86b9a2a313c3466b
MosabAboidrees <mosab_mahala@yahoo.com>
2023-10-19 13:08:58 -0700

    print with assembly

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,14 @@
-section .data
-hello db 'Hello, Holberton', 0  ; Define the string to be printed
-fmt db '%s', 10, 0  	    ; Define the format string for printf
+section	.data
+	hello db 'Hello, Holberton', 0  ; Define the string to be printedd
+	fmt db '%s', 10, 0  	    ; Define the format string for printf
 
 section .text
-extern printf  	; Declare the printf function
-global main  	; Define the entry point of the program
+	extern printf  	; Declare the printf function
+	global main  	; Define the entry point of the program
 main:
-    sub rsp, 8  	; Adjust the stack pointer
-    mov rdi, fmt  	; Set the format string argument for printf
-    mov rsi, hello  	; Set the string argument for printf
-    call printf  	; Call the printf function
-    add rsp, 8  	; Restore the stack pointer
-    ret  		; Return from the main function
+	sub rsp, 8  	; Adjust the stack pointer
+	mov rdi, fmt  	; Set the format string argument for printf
+	mov rsi, hello  	; Set the string argument for printf
+	call printf  	; Call the printf function
+	add rsp, 8  	; Restore the stack pointer
+	ret  		; Return from the main function
