ff8afa0b3b4408cd4c453e05a06180f0e2a30c76
MosabAboidrees <mosab_mahala@yahoo.com>
2023-10-19 13:19:12 -0700

    print with assembly

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,14 @@
 section .data
-    hello db 'Hello, Holberton', 0
-    fmt db '%s', 10, 0
+hello:	db 'Hello, Holberton', 0
+fmt:	db '%s', 10, 0
 
-section .text
-    extern printf
-
-    global main
+	section .text
+	extern printf
+	global main
 main:
-    sub rsp, 8
-    mov rdi, fmt
-    mov rsi, hello
-    call printf
-    add rsp, 8
-    ret
+	sub rsp, 8
+	mov rdi, fmt
+	mov rsi, hello
+	call printf
+	add rsp, 8
+	ret
