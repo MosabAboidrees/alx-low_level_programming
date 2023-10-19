0b82c71db7c44b294a7c60af2463c832f83d79b5
MosabAboidrees <mosab_mahala@yahoo.com>
2023-10-19 12:07:04 -0700

    number of elements

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,15 +2,14 @@
 
 /**
  * list_len - Returns the number of elements in a linked list_t list
- * @h: Pointer to the list
- *
+ * @h: singly linked list.
  * Return: The number of elements in the linked list
  */
 size_t list_len(const list_t *h)
 {
 	size_t NodesNum;
 
-	for (NodesNum = 0; current != NULL; NodesNum++)
+	for (NodesNum = 0; h != NULL; NodesNum++)
 	{
 		h = h->next;
 	}
