435ac59f07775b210fa188e81f577fb17e271756
MosabAboidrees <mosab_mahala@yahoo.com>
2023-10-19 11:57:48 -0700

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,16 +10,15 @@
 
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
+	size_t ListNum;
 
-    while (h != NULL)
-    {
-        if (h->str == NULL)
-            printf("[0] (nil)\n");
-        else
-            printf("[%d] %s\n", h->len, h->str);
-        count++;
-        h = h->next;
-    }
-    return (count);
+	for (ListNum = 0; h != NULL; ListNum++)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+	}
+	return (ListNum);
 }
