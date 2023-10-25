536d84d860a7f85d9c46f7937e5371bc27652b77
MosabAboidrees <mosab_mahala@yahoo.com>
2023-10-25 16:02:10 -0700

    a new node at the beginning of a list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -6,7 +6,7 @@
  * @n: new head value.
  * Return: The address of the new element, or NULL if it failed
  */
-listint_t *add_nodeint(listint_t **head, const n)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new_head;
 
