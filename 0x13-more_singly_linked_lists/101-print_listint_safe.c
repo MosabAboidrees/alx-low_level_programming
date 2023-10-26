02acb74eaf8870d3830537bc59a08ef130377c95
MosabAboidrees <mosab_mahala@yahoo.com>
2023-10-26 14:52:06 -0700

    prints a linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,36 +1,73 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - a function that prints a listint_t linked list
- * @head: Pointer to the head of the list
- * Return: The number of nodes in the list
+ * _realloc_list - Reallocates memory for an array of pointers to the nodes
+ * in a linked list.
+ * @old_list: The old list of pointers.
+ * @size: The new size of the list (always one more than the old size).
+ * @new_node: The new node to add to the list.
+ * Return: Pointer to the new list.
+ */
+const listint_t **_realloc_list(const listint_t **old_list, size_t size,
+		const listint_t *new_node)
+{
+	const listint_t **new_list;
+	size_t i;
+
+	/* Allocate memory for the new list */
+	new_list = malloc(size * sizeof(listint_t *));
+	if (new_list == NULL)
+	{
+		free(old_list); /*Free the old list*/
+		exit(98); /*exit if allocation fails*/
+	}
+	/* Copy elements from the old list to the new list */
+	for (i = 0; i < size - 1; i++)
+		new_list[i] = old_list[i];
+	/* Add the new node to the new list */
+	new_list[i] = new_node;
+	/* Free the old list as it is no longer needed */
+	free(old_list);
+	return (new_list);
+}
+
+/**
+ * print_listint_safe -  a function that prints a listint_t linked list safely
+ * @head: Pointer to the start of the list.
+ * Return: The number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t count = 0;
-	const listint_t *slow, *fast;
+	size_t index, node_count = 0;
+	const listint_t **address_list = NULL;
 
-	if (head == NULL)
-		exit(98); /*Exit with status 98 if the list is empty*/
-
-	slow = head; /* Initialize slow pointer to the head of the list*/
-	fast = head; /* Initialize fast pointer to the head of the list*/
-
-	/*Iterate through the list until the end or loop is detected*/
-	while (slow && fast && fast->next)
+	/* Traverse the list until the end is reached or a loop is detected */
+	while (head != NULL)
 	{
-		printf("[%p] %d\n", (void *)slow, slow->n); /*Print the current node*/
-		count++; /*Increment the node count*/
-
-		slow = slow->next; /*Move slow pointer one step forward*/
-		fast = fast->next->next; /*Move fast pointer two steps forward*/
-		/*Check if the pointers meet, indicating a loop*/
-		if (slow == fast)
+		/* Check if the current node has already been visited */
+		for (index = 0; index < node_count; index++)
 		{
-			/*Print the start of the loop*/
-			printf("-> [%p] %d\n", (void *)slow, slow->n);
-			break; /*Break the loop to avoid infinite printing*/
+			/* If a loop is detected, print the current node*/
+			/*and free memory before returning */
+			if (head == address_list[index])
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free(address_list);
+				return (node_count);
+			}
 		}
+
+		/* Increment the count of nodes and update the list of addresses */
+		/*address_list is used to store the addresses of the nodes that have*/
+		/*already been visited during the traversal of the linked list*/
+		node_count++;
+		address_list = _realloc_list(address_list, node_count, head);
+		/* Print the current node's address and data */
+		printf("[%p] %d\n", (void *)head, head->n);
+		/* Move to the next node in the list */
+		head = head->next;
 	}
-	return (count); /*Return the number of nodes in the list*/
+	/* Free the list of addresses and return the total number of nodes */
+	free(address_list);
+	return (node_count);
 }
