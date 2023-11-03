58136683a39baca85bfd58e86d828597176d2ff7
MosabAboidrees <mosab_mahala@yahoo.com>
2023-11-03 15:14:04 -0700

    copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,7 +1,10 @@
 #include "main.h"
 
 #define PERM (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
-
+#define USAGE "Usage: cp file_from file_to\n"
+#define E_N_READ "Error: Can't read from file %s\n"
+#define E_N_WRITE "Error: Can't write to %s\n"
+#define E_N_CLOSE "Error: Can't close fd %d\n"
 /**
  * main - copies the content of a file to another file
  * @argc: number of arguments
@@ -9,43 +12,43 @@
  *
  * Return:0 n success, or an exit code on failure
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int file_from = 0, file_to = 0, read_count, write_count;
-	char buffer[1024];
+	int file_from = 0, file_to = 0;
+	ssize_t  read_count;
+	char buffer[READ_BUFFER_SIZE];
 
 	/* Check the number of arguments */
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]), exit(97);
-	}
+		dprintf(STDERR_FILENO, USAGE), exit(97);
+
 	/* Open the source file for reading */
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		dprintf(STDERR_FILENO, E_N_READ, argv[1]), exit(98);
+
 	/* Open or create the destination file for writing */
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, PERM);
 	if (file_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-	/* Copy the content from source file to the destination file */
-	while ((read_count = read(file_from, buffer, 1024)) > 0)
-		write_count = write(file_to, buffer, read_count);
-		if (write_count != read_count)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	if (read_count == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+		dprintf(STDERR_FILENO, E_N_WRITE, argv[2]), exit(99);
 
-	/* Close the file descriptors */
-	CFF = close(file_from), CFT = close(file_to;
-	if (CFF == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", CFF), exit(100);
-	if (CFT == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", CFT), exit(100);
-	return (0);
+	/* Copy the content from source file to the destination file */
+	while ((read_count = read(file_from, buffer, READ_BUFFER_SIZE)) > 0)
+	{
+		if (write(file_to, buffer, read_count) != read_count)
+		{
+			dprintf(STDERR_FILENO, E_N_WRITE, argv[2]), exit(99);
+		}
+	}
+	if (read_count == -1)
+	{
+		dprintf(STDERR_FILENO, E_N_READ, argv[1]), exit(98);
+	}
+	file_from = close(file_from), file_to = close(file_to);
+	if (file_from == -1)
+		dprintf(STDERR_FILENO, E_N_CLOSE, file_from), exit(100);
+	if (file_to == -1)
+		dprintf(STDERR_FILENO, E_N_CLOSE, file_from), exit(100);
+
+	return (EXIT_SUCCESS);
 }
