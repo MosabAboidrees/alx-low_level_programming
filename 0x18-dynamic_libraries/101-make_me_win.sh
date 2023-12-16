725ab793f4317a6c3ef6385f7cb4c0047a9a4cb7
MosabAboidrees <mosab_mahala@yahoo.com>
2023-12-15 17:28:13 -0800

    Hacking Injection

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -N -P /tmp/libhack.so https://github.com/MosabAboidrees/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libhack.so
-export LD_PRELOAD=/tmp/libhack.so
+gcc -shared -o libhack.so -fPIC winning_numbers.c
+export LD_PRELOAD=./libhack.so:$LD_LIBRARY_PATH
