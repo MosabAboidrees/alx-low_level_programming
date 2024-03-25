b49fe474de248c43aab7133fb2e33ce462a94d7a
MosabAboidrees <mosab_mahala@yahoo.com>
2024-03-25 16:36:29 -0700

    dynamic library

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# gcc -c -Fpic *.c
-# gcc -shared -o liball.so *.o
-gcc -fPIC -c *.c
-gcc -shared *.o -o liball.so
--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-gcc -shared -o libhack.so -fPIC winning_numbers.c
-export LD_PRELOAD=./libhack.so:$LD_LIBRARY_PATH
